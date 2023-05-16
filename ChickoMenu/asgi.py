b762b193e5bc37298f71df4135c9ddefff99f788
oldcorvus <moelcrow@gmail.com>
2023-05-16 08:31:05 +0200

    rename project

--- ChickoMenu/asgi.py
+++ ChickoMenu/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ChikoMenu project.
+ASGI config for ChickoMenu project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChikoMenu.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChickoMenu.settings')
 
 application = get_asgi_application()
