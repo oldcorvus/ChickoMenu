97fe8f297b14fd2fbb1884777fb4d27401e11568
oldcorvus <moelcrow@gmail.com>
2023-05-20 22:26:17 +0200

    Added test for accounts models

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,22 +1,23 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+from django.urls import reverse
+from ..models import User
 
-class UserTest(TestCase):
-    """ Test module for User model """
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser", first_name="Test", last_name="User",
+                                        phone_number="1234567890", email="testuser@example.com",
+                                        is_active=True, is_admin=False, is_staff=False)
 
-    def test_create_user(self):
-        user = get_user_model().objects.create(username="moel",phone_number='09909900999'\
-            ,email="normal@user.com",password="passwordsimple")
-        self.assertEqual(user.email, 'normal@user.com')
-        self.assertFalse(user.is_active)
+    def test_user_creation(self):
+        user = User.objects.get(username="testuser")
+        self.assertEqual(user.first_name, "Test")
+        self.assertEqual(user.last_name, "User")
+        self.assertEqual(user.phone_number, "1234567890")
+        self.assertEqual(user.email, "testuser@example.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_admin)
         self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
 
-    def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser(
-            username="moel", phone_number="123456789", email='super@user.com', password='foo')
-        self.assertEqual(admin_user.email, 'super@user.com')
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
+    def test_str_representation(self):
+        user = User.objects.get(username="testuser")
+        self.assertEqual(str(user), "testuser--1234567890")
