15db217d4fb772cc73e4827b681a590a98b776aa
oldcorvus <moelcrow@gmail.com>
2023-05-21 19:50:31 +0200

    Added UserSerializer

---
+++ accounts/api/serializers.py
@@ -0,0 +1,58 @@
+from django.utils.translation import gettext as _
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator, BaseUniqueForValidator
+from utils.mixins import SetCustomErrorMessagesMixin
+from django.contrib.auth.hashers import make_password
+from .validators import check_phone_number
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+class UserSerializer(SetCustomErrorMessagesMixin, serializers.ModelSerializer):
+    """Serializer for the users object"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'username', 'phone_number',
+                  'first_name', 'last_name', 'profile_image')
+        read_only_fields = ("id",)
+        extra_kwargs = {
+            'username': {"error_messages": {"blank": _("Username cannot be blank.")}},
+            'password': {"error_messages": {"blank": _("Password cannot be blank.")}},
+            'phone_number': {"error_messages": {"blank": _("Phonenumber cannot be blank.")}},
+        }
+        custom_error_messages_for_validators = {
+            'username': {
+                UniqueValidator: _('This username has already been taken. Please choose a different one.'),
+                BaseUniqueForValidator: _('Username cannot be blank.'),
+            },
+            'phone_number': {
+                UniqueValidator: _('This phone number has already been taken. Please choose a different one.'),
+                BaseUniqueForValidator: _('Phone number cannot be blank.'),
+            }
+        }
+
+    def validate(self, data):
+        # get the password from the data
+        password = data.get('password')
+
+        # validate password
+        try:
+            validate_password(password=password, user=self.instance)
+        except ValidationError:
+            raise serializers.ValidationError(_("Password must be at least 8 characters."))
+
+        return super().validate(data)
+
+    def validate_phone_number(self, value):
+        # validate phone number
+        # use a custom phone number validation function
+        return check_phone_number(value)
+
+    def create(self, validated_data):
+        # hash password
+        password = validated_data.pop('password')
+        hashed_password = make_password(password)
+        validated_data['password'] = hashed_password
+
+        return super().create(validated_data)
