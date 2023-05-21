5d638f79dcc0082eab07b0be6da59762e024394d
oldcorvus <moelcrow@gmail.com>
2023-05-22 00:27:55 +0200

    Added theme model

---
+++ theme/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from utils.models import TimeStampedUUIDModel
+class Theme(TimeStampedUUIDModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    preview = models.ImageField(upload_to='theme_preview')
+    background_image = models.ImageField(upload_to='theme_image')
+    font_family = models.CharField(max_length=255, default='Arial, sans-serif')
+    menu_background_color = models.CharField(max_length=7, default='#f8f9fa')
+    menu_text_color = models.CharField(max_length=7, default='#343a40')
+
+    def __str__(self):
+        return self.name
