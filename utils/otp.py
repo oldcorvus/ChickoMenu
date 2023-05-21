ab46f0ac8d901c2eaaa5707ce353c963304e700c
oldcorvus <moelcrow@gmail.com>
2023-05-21 21:34:47 +0200

    Added get_client_ip function

--- utils/otp.py
+++ utils/otp.py
@@ -5,7 +5,14 @@ from kavenegar import *
 
 from django.conf import settings
 
-
+def get_client_ip(request) :
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
+    
     
 def otp_generator(size: int = 6, char: str = digits) :
     return "".join(secret_choice(char) for _ in range(size))
