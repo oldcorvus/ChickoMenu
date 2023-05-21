bdbf62e8c44cb89ef717a96da0982f87150c603a
oldcorvus <moelcrow@gmail.com>
2023-05-21 21:57:29 +0200

    Added url for VerifyOtp api

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Register
+from .views import Register,VerifyOtp
 
 app_name = "api"
 
 urlpatterns = [
     path("register/", Register.as_view(), name="register"),
+    path("verify/", VerifyOtp.as_view(), name="verify-otp"),
 ]
