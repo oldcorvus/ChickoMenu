e6a316cf49c2c7fcc2b3a69dd78f26211589c6e1
oldcorvus <moelcrow@gmail.com>
2023-05-22 19:45:21 +0200

    Added ThemeListCreateAPIView and ThemeRetrieveUpdateDestroyAPIView

--- theme/models.py
+++ theme/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from utils.models import TimeStampedUUIDModel
+
 class Theme(TimeStampedUUIDModel):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    preview = models.ImageField(upload_to='theme_preview')
-    background_image = models.ImageField(upload_to='theme_image')
+    preview = models.ImageField(upload_to='theme_preview',blank=True, null=True)
+    background_image = models.ImageField(upload_to='theme_image', blank=True, null=True)
     font_family = models.CharField(max_length=255, default='Arial, sans-serif')
     menu_background_color = models.CharField(max_length=7, default='#f8f9fa')
     menu_text_color = models.CharField(max_length=7, default='#343a40')
