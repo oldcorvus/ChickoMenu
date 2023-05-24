f2ebcbedc6cf6284f156bab3e67cb42240302248
oldcorvus <moelcrow@gmail.com>
2023-05-24 18:16:52 +0200

    Added Menu, MenuItem, Category to admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import   Menu,  MenuItem, Category
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(Category)
+admin.site.register(MenuItem)
