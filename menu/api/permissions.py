6a1593148c7e69acbcfbdb74c8f95399a6c5234c
oldcorvus <moelcrow@gmail.com>
2023-05-24 23:58:08 +0200

    Added MenuOwnerOrReadOnly permission

--- menu/api/permissions.py
+++ menu/api/permissions.py
@@ -13,3 +13,24 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     return obj.owner == request.user
 
 
+class MenuOwnerOrReadOnly(permissions.BasePermission):
+      
+  def has_object_permission(self, request, view, obj):
+    # Read permissions are allowed to any request,
+    # so we'll always allow GET, HEAD or OPTIONS requests.
+    if request.method in permissions.SAFE_METHODS:
+        return True
+
+    # Write permissions are only allowed to the owner of the place.
+    return obj.menu.owner == request.user
+
+  def has_permission(self, request, view):
+    if request.method == "POST":
+        # For create action
+        try:
+            menu_id = request.data["menu"]
+            menu = Menu.objects.get(pk=menu_id, owner=request.user)
+            return True
+        except:
+            return False
+    return True
