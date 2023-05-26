0271c1db9eb73ff21b20f17e60712486c6b1e2ad
oldcorvus <moelcrow@gmail.com>
2023-05-26 14:38:10 +0200

    Added url for MenuDetail

--- menu/api/urls.py
+++ menu/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ListOfAllActiveMenus, UserMenus
+from .views import ListOfAllActiveMenus, MenuDetail, UserMenus
 
 app_name = "menu"
 
 urlpatterns = [
     path('menus/active/', ListOfAllActiveMenus.as_view(), name='list_active_menus'),
     path('menus/user/', UserMenus.as_view(), name='user_menus'),
+    path('menus/<str:pk>', MenuDetail.as_view(),name='menu_detail'),
 
 ]
