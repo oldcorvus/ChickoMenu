b05cc9cf6d67b400ffb072bd8c871085fbfbedc4
oldcorvus <moelcrow@gmail.com>
2023-05-30 21:47:41 +0200

    Added tests for Plan model

---
+++ plan/tests/test_models.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+from plan.models import Plan, PlanItem
+
+class PlanModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.plan_item1 = PlanItem.objects.create(
+            name='Item 1',
+            description='This is item 1'
+        )
+        cls.plan_item2 = PlanItem.objects.create(
+            name='Item 2',
+            description='This is item 2'
+        )
+        cls.plan = Plan.objects.create(
+            name='Basic Plan',
+            description='This is a basic plan',
+            price=10.0
+        )
+        cls.plan.features.add(cls.plan_item1)
+        cls.plan.features.add(cls.plan_item2)
+
+    def test_plan_name(self):
+        plan = Plan.objects.get(id=self.plan.id)
+        self.assertEqual(plan.name, 'Basic Plan')
+
+    def test_plan_description(self):
+        plan = Plan.objects.get(id=self.plan.id)
+        self.assertEqual(plan.description, 'This is a basic plan')
+
+    def test_plan_price(self):
+        plan = Plan.objects.get(id=self.plan.id)
+        self.assertEqual(plan.price, 10.0)
+
+    def test_plan_str(self):
+        plan = Plan.objects.get(id=self.plan.id)
+        self.assertEqual(str(plan), 'Basic Plan')
+
+    def test_plan_items(self):
+        plan = Plan.objects.get(id=self.plan.id)
+        self.assertEqual(list(plan.features.all()), [self.plan_item1, self.plan_item2])
+
+    def test_plan_item_name(self):
+        plan_item = PlanItem.objects.get(id=self.plan_item1.id)
+        self.assertEqual(plan_item.name, 'Item 1')
+
+    def test_plan_item_description(self):
+        plan_item = PlanItem.objects.get(id=self.plan_item1.id)
+        self.assertEqual(plan_item.description, 'This is item 1')
+
+    def test_plan_item_str(self):
+        plan_item = PlanItem.objects.get(id=self.plan_item1.id)
+        self.assertEqual(str(self.plan_item1), 'Item 1')
