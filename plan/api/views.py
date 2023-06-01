f1a2cc10d53664c5c724d9fc6da9ae09be3eb29d
oldcorvus <moelcrow@gmail.com>
2023-06-01 14:14:37 +0200

    Added PlanViewSet

---
+++ plan/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from plan.models import Plan, PlanItem
+from plan.api.serializers import PlanSerializer, PlanItemSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class PlanViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Plan.objects.prefetch_related('features').all()
+    serializer_class = PlanSerializer
