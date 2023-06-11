e843e3a2d26de618c35d91aac26c2afc289014fa
oldcorvus <moelcrow@gmail.com>
2023-06-11 21:04:41 +0200

    Updated tests for Theme model

--- theme/tests/test_models.py
+++ theme/tests/test_models.py
@@ -7,27 +7,40 @@ class ThemeModelTest(TestCase):
     def setUpTestData(cls):
         # Create a sample Theme object for testing
         preview = SimpleUploadedFile(name='test_preview.png', content=b'', content_type='image/png')
-        background_image = SimpleUploadedFile(name='test_background.png', content=b'', content_type='image/png')
+        header_image = SimpleUploadedFile(name='test_background.png', content=b'', content_type='image/png')
+        logo_image = SimpleUploadedFile(name='test_background.png', content=b'', content_type='image/png')
+
         theme = Theme.objects.create(
             id = 1,
             name='Test Theme',
-            description='A test theme for testing purposes',
             preview=preview,
-            background_image=background_image,
+            header_image=header_image,
+            logo_image = logo_image,
+            header_color='#343a40',
             font_family='Times New Roman',
             menu_background_color='#ffffff',
             menu_text_color='#000000',
         )
+        
+    def test_theme_logo_image_upload_to(self):
+        theme = Theme.objects.get(id=1)
+        expected_upload_to = 'theme_image'
+        self.assertEqual(expected_upload_to, theme.logo_image.field.upload_to)
+
+    def test_theme_header_color(self):
+        theme = Theme.objects.get(id=1)
+        expected_header_color = '#343a40'
+        self.assertEqual(expected_header_color, theme.header_color)
+
+    def test_theme_header_image_upload_to(self):
+        theme = Theme.objects.get(id=1)
+        expected_upload_to = 'theme_image'
+        self.assertEqual(expected_upload_to, theme.header_image.field.upload_to)
 
     def test_theme_name(self):
         theme = Theme.objects.get(id=1)
         expected_name = f"{theme.name}"
         self.assertEqual(expected_name, str(theme))
-
-    def test_theme_description(self):
-        theme = Theme.objects.get(id=1)
-        expected_description = 'A test theme for testing purposes'
-        self.assertEqual(expected_description, theme.description)
 
     def test_theme_font_family(self):
         theme = Theme.objects.get(id=1)
@@ -48,8 +61,3 @@ class ThemeModelTest(TestCase):
         theme = Theme.objects.get(id=1)
         expected_upload_to = 'theme_preview'
         self.assertEqual(expected_upload_to, theme.preview.field.upload_to)
-
-    def test_theme_background_image_upload_to(self):
-        theme = Theme.objects.get(id=1)
-        expected_upload_to = 'theme_image'
-        self.assertEqual(expected_upload_to, theme.background_image.field.upload_to)
