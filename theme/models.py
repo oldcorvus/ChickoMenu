8579367de9ce449b9fd2807f05199cb0583e6102
oldcorvus <moelcrow@gmail.com>
2023-06-11 21:04:02 +0200

    Added logo_image and header_color and renamed background_image to header_image

--- theme/models.py
+++ theme/models.py
@@ -3,12 +3,13 @@ from utils.models import TimeStampedUUIDModel
 
 class Theme(TimeStampedUUIDModel):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
     preview = models.ImageField(upload_to='theme_preview',blank=True, null=True)
-    background_image = models.ImageField(upload_to='theme_image', blank=True, null=True)
     font_family = models.CharField(max_length=255, default='Arial, sans-serif')
     menu_background_color = models.CharField(max_length=7, default='#f8f9fa')
     menu_text_color = models.CharField(max_length=7, default='#343a40')
+    logo_image = models.ImageField(upload_to='theme_image', blank=True, null=True)
+    header_color = models.CharField(max_length=7, default='#343a40')
+    header_image = models.ImageField(upload_to='theme_image', blank=True, null=True)
 
     def __str__(self):
         return self.name
