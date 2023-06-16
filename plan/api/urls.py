19ededbc772482d21913eef133216fae1d808c4d
oldcorvus <moelcrow@gmail.com>
2023-06-16 16:46:25 +0200

    Added url UserPlanCreateAPIView and UserPlansAPIView

--- plan/api/urls.py
+++ plan/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from plan.api.views import PlanViewSet, PlanItemViewSet
+from plan.api.views import PlanViewSet, PlanItemViewSet, UserPlanCreateAPIView, UserPlansAPIView
 
 router = DefaultRouter()
 router.register(r'plans', PlanViewSet, basename='plan')
@@ -10,4 +10,6 @@ app_name = "plan"
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('user-plans/create/', UserPlanCreateAPIView.as_view(), name='user_plan_create'),
+    path('user-plans/<str:user_id>/', UserPlansAPIView.as_view(), name='user_plans'),
 ]
