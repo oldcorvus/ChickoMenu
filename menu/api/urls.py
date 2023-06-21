27e8d2ce2de14817ce89045d10edf3212b80f8bc
oldcorvus <moelcrow@gmail.com>
2023-06-21 10:24:01 +0200

    Added urls for CreateCategory and CreateMenuItem api view

--- menu/api/urls.py
+++ menu/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ListOfAllActiveMenus, MenuDetail, MenuItemDetail, UserMenus, CategoryDetail
+from .views import ListOfAllActiveMenus, MenuDetail, MenuItemDetail, UserMenus, CategoryDetail\
+    ,CreateCategory, CreateMenuItem
 
 app_name = "menu"
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('menus/<str:pk>', MenuDetail.as_view(),name='menu_detail'),
     path('categories/<str:pk>/', CategoryDetail.as_view(), name='category-detail'),
     path('menu-item/<str:pk>/', MenuItemDetail.as_view(), name='menu-item-detail'),
-
+    path('menus/categories/create/', CreateCategory.as_view(), name='category-create'),
+    path('menus/menuitems/create/', CreateMenuItem.as_view(), name='create-menu-item'),
 ]
