c32da55ed5f7e38aeaf97cacb4ed7d6ad929d1c4
oldcorvus <moelcrow@gmail.com>
2023-06-23 22:26:22 +0200

    Updated default values for Theme

--- theme/models.py
+++ theme/models.py
@@ -4,11 +4,11 @@ from utils.models import TimeStampedUUIDModel
 class Theme(TimeStampedUUIDModel):
     name = models.CharField(max_length=255)
     preview = models.ImageField(upload_to='theme_preview',blank=True, null=True)
-    font_family = models.CharField(max_length=255, default='Arial, sans-serif')
-    menu_background_color = models.CharField(max_length=7, default='#f8f9fa')
-    menu_text_color = models.CharField(max_length=7, default='#343a40')
+    font_family = models.CharField(max_length=255, default='Vazir')
+    menu_background_color = models.CharField(max_length=7, default='#fff')
+    menu_text_color = models.CharField(max_length=7, default='#000')
     logo_image = models.ImageField(upload_to='theme_image', blank=True, null=True)
-    header_color = models.CharField(max_length=7, default='#343a40')
+    header_color = models.CharField(max_length=7, default='#2196f3')
     header_image = models.ImageField(upload_to='theme_image', blank=True, null=True)
 
     def __str__(self):
