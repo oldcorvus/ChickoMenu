10ac44628ad4f5a62cde639e29050239bc23fbb6
oldcorvus <moelcrow@gmail.com>
2023-06-29 21:45:44 +0200

    Change update method of ThemeSerializer

--- theme/api/serializers.py
+++ theme/api/serializers.py
@@ -9,3 +9,20 @@ class ThemeSerializer(serializers.ModelSerializer):
         fields = ('id','name',  'preview', 'logo_image',\
              'font_family', 'menu_background_color', 'menu_text_color',\
                  'header_image', 'header_color' )
+
+    def update(self, instance, validated_data):
+        logo_image = validated_data.pop('logo_image', None)
+        preview = validated_data.pop('preview', None)
+        header_image = validated_data.pop('header_image', None)
+        theme = super().update(instance, validated_data)
+        if logo_image:
+            theme.logo_image.delete()  # delete previous logo image
+            theme.logo_image = logo_image
+        if preview:
+            theme.preview.delete()  # delete previous preview image
+            theme.preview = preview
+        if header_image:
+            theme.header_image.delete()  # delete previous header image
+            theme.header_image = header_image
+        theme.save()
+        return theme
