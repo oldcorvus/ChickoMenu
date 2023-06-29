5eb7f67eb15b1c081dd301fbdf4799ce583ca093
oldcorvus <moelcrow@gmail.com>
2023-06-29 22:40:40 +0200

    Added IsAdminOrReadOnly permission

---
+++ plan/api/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
