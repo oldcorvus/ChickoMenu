820667e99eb15dd5cdbb76baa53d4c8fe8ba29e3
oldcorvus <moelcrow@gmail.com>
2023-06-29 22:41:03 +0200

    Changed permissions for PlanViewSet and PlanItemViewSet

--- plan/api/views.py
+++ plan/api/views.py
@@ -2,15 +2,16 @@ from rest_framework import viewsets
 from plan.models import Plan, PlanItem, UserPlan
 from plan.api.serializers import PlanSerializer, PlanItemSerializer,UserPlanSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAdminOrReadOnly
 from rest_framework import generics, permissions
 
 class PlanViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminOrReadOnly]
     queryset = Plan.objects.prefetch_related('features').all()
     serializer_class = PlanSerializer
 
 class PlanItemViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes =  [IsAuthenticatedOrReadOnly, IsAdminOrReadOnly]
     queryset = PlanItem.objects.all()
     serializer_class = PlanItemSerializer
 
