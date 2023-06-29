b6717ec0df3e33653ae328af13f0106501ff1dd9
oldcorvus <moelcrow@gmail.com>
2023-06-29 21:45:14 +0200

    Change permissions for ThemeRetrieveUpdateDestroyAPIView

--- theme/api/views.py
+++ theme/api/views.py
@@ -1,15 +1,15 @@
 from rest_framework import generics
 from theme.models import Theme
 from .serializers import ThemeSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class ThemeListCreateAPIView(generics.ListCreateAPIView):
     queryset = Theme.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = ThemeSerializer
 
 class ThemeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Theme.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = ThemeSerializer
