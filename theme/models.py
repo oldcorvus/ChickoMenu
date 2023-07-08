bbe82abe432dd34deea520d10777ca837734f755
oldcorvus <moelcrow@gmail.com>
2023-07-08 17:54:17 +0200

    Moved files

--- theme/models.py
+++ theme/models.py
@@ -5,11 +5,13 @@ class Theme(TimeStampedUUIDModel):
     name = models.CharField(max_length=255)
     preview = models.ImageField(upload_to='theme_preview',blank=True, null=True)
     font_family = models.CharField(max_length=255, default='Vazir')
-    menu_background_color = models.CharField(max_length=7, default='#fff')
-    menu_text_color = models.CharField(max_length=7, default='#000')
+    menu_background_color = models.CharField(max_length=7, default='#ffffff')
+    menu_text_color = models.CharField(max_length=7, default='#000000')
     logo_image = models.ImageField(upload_to='theme_image', blank=True, null=True)
     header_color = models.CharField(max_length=7, default='#2196f3')
     header_image = models.ImageField(upload_to='theme_image', blank=True, null=True)
+    menu_item_background_color =  models.CharField(max_length=7, default='#ffffff')
+
 
     def __str__(self):
         return self.name
