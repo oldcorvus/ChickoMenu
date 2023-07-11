d6398c85099f97ffccdffbeeb171ac7526ea1600
oldcorvus <moelcrow@gmail.com>
2023-07-11 17:25:48 +0200

    Updated settings for production

--- ChickoMenu/settings/production.py
+++ ChickoMenu/settings/production.py
@@ -2,30 +2,48 @@ from .base import *  # noqa
 
 SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-STATIC_ROOT = env("STATIC_ROOT")
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-
+DEBUG = False
 REST_FRAMEWORK = {
     **REST_FRAMEWORK,
     "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
 }
 
-CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200'
+]
+DATABASES = {
+"default": {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": "postgres",
+    "USER": "postgres",
+    "PASSWORD": "postgres",
+    "HOST": "db",
+    "PORT": 5432,
+    }
+}
 
-INSTALLED_APPS = INSTALLED_APPS + ["monitus"]
-
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 MIDDLEWARE = MIDDLEWARE + [
     "monitus.middleware.FailedLoginMiddleware",
     "monitus.middleware.Error403EmailsMiddleware",
 ]
 
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = env("EMAIL_PORT")
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = env("EMAIL_HOST", default='')
+EMAIL_PORT = env("EMAIL_PORT",  default='')
+EMAIL_HOST_USER = env("EMAIL_HOST_USER",  default='')
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD",  default='')
 EMAIL_USE_TLS = True
 
 
-ADMINS = [("Your name here", "your@email.here")]  # CHANGE THIS WITH YOUR EMAIL
+ADMINS = [("m", "moelcrow@email.here")]  # CHANGE THIS WITH YOUR EMAIL
--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV PIP_DISABLE_PIP_VERSION_CHECK 1
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 # Set work directory
-WORKDIR /code
+WORKDIR /app
 
 #RUN apt-get update && apt-get install libgdal-dev
 #RUN export CPLUS_INCLUDE_PATH=/usr/include/gdal
